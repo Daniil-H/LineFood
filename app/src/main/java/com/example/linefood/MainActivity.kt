f4d0c469d62b60c3b8cfdfdee370c6f118d48b81
Daniil-H <harkovdaniil22@gmail.com>
2023-05-13 10:05:06 +0500

    12

---
+++ app/src/main/java/com/example/linefood/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.linefood
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val timer = object: CountDownTimer(3000,1000){
+            override fun onTick(p0: Long) {
+
+            }
+
+            override fun onFinish(){
+                val intent = Intent(this@MainActivity, MenuActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        timer.start()
+    }
+}
